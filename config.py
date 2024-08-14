065720a1cc74400a739b94b715de3d161ed8bc9f
HaixiaHan <154297799+haixiaHan99@users.noreply.github.com>
2024-08-14 16:46:46 +0800

    update the inference of UCE

---
+++ config.py
@@ -0,0 +1,6 @@
+prompt_templates = {
+    "GSM_conf": f"Here is a math problem and some steps:\n hhx \nPlease give your confidence.",
+    "GSM_format":f"Here is a math probem:\n hhx \nPlease think step by step",
+    "CSQA_conf": f"Below is a question and some steps:\nQuestion:\n{{question}}Please give your confidence.",
+    "CSQA_format":f"Below is a question, analyze step by step and select one correct answer from it:\nQuestion:\n{{question}}"
+}
